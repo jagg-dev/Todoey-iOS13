f3d8cf6c94aead62865460ddc8eae9689399c743
Jorge Gonzalez <jagg.dev.27@gmail.com>
2022-06-09 00:13:22 -0500

    Create Item model. Add MVC structure.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Jorge Gonzalez on 08/06/22.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
